d714228e2cb6fe8de06f48035b1ac904f0774fcc
Jordi Alejandro García Núñez <jordign.mx@hotmail.com>
2015-04-17 10:00:53 -0500

    17/04/2015
    Creación de carpeta de unidad 4 para trabajar PHP puro, agrgamos, se
    crearon las carpetas de img, css y js donde metimos el archivo de la
    versión de jquery, se hizo el archivo index.php donde pusimos listas con
    items, secciones y divisiones,

---
+++ Unidad 4/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Document</title>
+  <script src="js/jquery-2.1.3.min.js"></script>
+  <script type="text/javascript" src="js/lib.js"></script>
+</head>
+<body>
+  <header>
+    <h1>Agenda telefónica</h1>
+  </header>
+  <nav>
+    <ul>
+      <li><a href=" ">Alta agenda</a></li>
+      <li><a href=" ">Consulta agenda</a></li>
+    </ul>
+  </nav>
+  <section>
+
+  </section>
+  <footer>
+
+  </footer>
+</body>
+</html>
